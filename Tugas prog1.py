05653c4cb771e83f527eb3746ce540c11993740a
jelyananandah <jelyanananda@gmail.com>
2024-10-25 10:17:02 +0800

    Add files via upload

---
+++ Tugas prog1.py
@@ -0,0 +1,7 @@
+print("Nama saya Jelyan Ananda Herdiyatma")
+print("Nim saya adalah 24241175")
+
+print("Saya Berasal dari Kota Bima")
+
+print("Nim pertama saya 24 + Nim terakhir 75")
+print("Jumlah dari dua dijit pertama dan dua digit terakhir 99")
