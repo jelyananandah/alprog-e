fddbb369e4317ccadaaaf932d459d244c9f203e6
jelyananandah <jelyanananda@gmail.com>
2024-11-05 15:47:42 +0800

    Add files via upload

---
+++ tugas 5.py
@@ -0,0 +1,11 @@
+# Tugas 5 Ananda
+
+nim =  24241175 
+
+nimAwal = 24
+nimAkhir = 75
+alas = nimAkhir
+tinggi  = nimAwal
+
+hasil = 0.5 * alas * tinggi
+print(f"hasil dari 1/2 x {alas} x {tinggi} adalah = {hasil}")
