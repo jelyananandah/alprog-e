e5fada6a0d0cada9d9a071f0a2df498fc416bdea
jelyananandah <jelyanananda@gmail.com>
2024-11-10 19:40:27 +0800

    Add files via upload

---
+++ prog6.py
@@ -0,0 +1,25 @@
+# variabel assigment value
+x = 9
+y = 4
+print("nilai dari x adalah : ", y)
+
+# aturan penamaan variabel 
+luas = 75 #
+ananda_h = 100 #
+AnandaH = 9 #
+anandah = 6
+El69 = 10 #
+print("nilai dari anadah adalaah : ", El69)
+
+
+a = 6 # interger bilangan bulat 
+print("tipe data dari a : ", type(a))
+
+b =  6,9 # Float bilangan pecahan
+print("tipe data dari b : ", type(b))
+
+c = "iqbaal" # string, gabungan karakter
+print("tipe data dari c : ", type(c))
+
+d = False # Boolen, true false
+print("tipe data dari d : ", type(d))
