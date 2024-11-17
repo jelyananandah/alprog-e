e69a03f387d62337d2bfafeea4a690bcd9783155
jelyananandah <jelyanananda@gmail.com>
2024-11-16 21:54:09 -0800

    Add files via upload

---
+++ prog7.py
@@ -0,0 +1,9 @@
+# Menghitung Segitiga 
+# nama : Jelyan Ananda Herdiyatma 
+
+print("===== Menghitung Segitiga =====")
+alas = int(input("Masukkan Alas : "))
+tinggi = int(input("Masukan Tinggi : "))
+
+hasil = 0,5 *alas *tinggi 
+print(f"Hasil dari 1/2 x {alas} x {tinggi} adalah : {hasil}")
