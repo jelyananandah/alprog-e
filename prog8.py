15cb49c0cc1d32c3dcafd3318c64d41b98d76830
jelyananandah <jelyanananda@gmail.com>
2024-12-01 01:08:53 +0800

    Add files via upload

---
+++ prog8.py
@@ -0,0 +1,32 @@
+#aritmatika
+
+x=24
+y=75
+
+#Operasi Penjumlahan
+hasil = x + y
+print ("nilai dari x + y = ",hasil)
+
+# Pengurangan
+hasil = x - y
+print("nilai dari x - y = ",hasil)
+
+# Perkalian
+hasil = x * y
+print("nilai dari x * y = ",hasil)
+
+# Pembagian
+hasil = x / y
+print("nilai dari x / y = ",hasil)
+
+# eksponen (perpangkatan)
+hasil = x ** y
+print("nilai dari x ** y = ",hasil)
+
+# Modulus (sisa bagi)
+hasil = x % y
+print("nilai dari x Mod y = ",hasil)
+
+# Floor Division
+hasil= x // y
+print("nilai dari x FD y = ",hasil)
