90069b9cb72ad4760aa02963f2d5232bcbcdffee
jelyananandah <jelyanananda@gmail.com>
2024-12-08 15:20:58 +0800

    Add files via upload

---
+++ tugas9.py
@@ -0,0 +1,12 @@
+#latihan operasi aritmatika
+
+nilai = float(input("masukkan nilai dalam suhu celcius : ",))
+
+fahrenheit =(nilai *9/5)+32
+print("nilai dalam fahranheit=",fahrenheit)
+
+kelvin = nilai + 273.15
+print("nilai dalam kelvin=",kelvin)
+
+roamer = nilai * (5/4)+0
+print("nilai dalam roamer=",roamer)
